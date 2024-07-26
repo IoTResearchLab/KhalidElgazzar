ab2780504f6e2015f1b1d0add5d894aef03188e0
elewah <abdelrahman.elewah@gmail.com>
2024-07-26 13:21:42 -0400

    fixing baseUrl issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://IoTResearchLab.github.io/KhalidElgazzar/',
+  url: 'https://IoTResearchLab.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/KhalidElgazzar',
   projectName: 'KhalidElgazzar',
   organizationName: 'IoTResearchLab',
   trailingSlash: false,
